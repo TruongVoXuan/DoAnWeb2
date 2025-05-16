a5284524e59d097e07caab5b6ae357340ca28d65
TruongVoXuan <truong.75.cvan@gmail.com>
2025-05-16 21:10:58 +0700

    tạo dự án

---
+++ AiLaTrieuPhu/src/main/java/truong/vx/AiLaTrieuPhu/AiLaTrieuPhuApplication.java
@@ -0,0 +1,13 @@
+package truong.vx.AiLaTrieuPhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLaTrieuPhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLaTrieuPhuApplication.class, args);
+	}
+
+}
---
+++ AiLaTrieuPhu/src/test/java/truong/vx/AiLaTrieuPhu/AiLaTrieuPhuApplicationTests.java
@@ -0,0 +1,13 @@
+package truong.vx.AiLaTrieuPhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLaTrieuPhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
